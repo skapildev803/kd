40b02510a60b46a410909ad15fab980546672dd7
skapildev803 <59012706+skapildev803@users.noreply.github.com>
2021-05-07 11:48:04 +0530

    Add files via upload

---
+++ Break.java
@@ -0,0 +1,185 @@
+
+public class Break {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	/*	int i=0;
+		
+		while(i<5)
+		{
+			
+			System.out.println("kapil dev sharma   " +i);
+			
+			
+			i++;
+			if(i==4) {
+				
+				break;
+			}
+			
+		}
+		
+		System.out.println("outside whille loop and at the end of the progream");
+		
+	
+		int i=2;
+		
+		switch(i) {
+		
+		case 0:
+			System.out.println("inside block 0");
+			break;
+			
+		case 1:
+			System.out.println("inside block 1");
+			break;
+			
+		case 2:
+			System.out.println("inside block 2");
+			break;
+			
+		case 3:
+			System.out.println("inside block 3");
+			break;
+			
+			default:
+			System.out.println("inside default block");
+			
+			
+				
+		int i=0;
+		
+		
+			while(i<10)
+			{
+				
+				
+				System.out.println("kapil dev sharma: " +i);
+				
+				i++;
+				
+				if(i==6) {
+					
+					break;
+				}
+				
+		
+		}
+			System.out.println("outside while loop and at the end of the program");
+		
+	
+		
+		
+		int i=0;
+		
+		do {
+		
+			System.out.println("kapil dev sharma :" +i);
+			
+			i++;
+			
+			if(i==5) {
+		
+				break;
+			}
+		}while(i<10);
+		
+		System.out.println("your are out side the while loop");
+		
+		
+		
+		
+
+		
+		
+	    for(int i=0;i<10;++i) {
+	    	
+	    	
+	    	System.out.println("the value of i is:" +i);
+	    	
+	    	
+	    	if(i==5)
+	    	{
+	    		break;
+	    	}
+	    	
+	    }
+					
+		
+		
+	int a=5,b=6;
+	
+	System.out.println("starting of the program");
+	
+	
+	kapil:{
+		
+		System.out.println("inside the labeled block");
+		System.out.println("the value of a is 5");
+		
+		if(a<b) {
+			break kapil;
+		}
+		System.out.println("the value of b is  6");
+		System.out.println("at the end of labled block");
+		
+	}
+	System.out.println("end of the program");
+	
+
+		
+		for(int i=0;i<10;++i) {
+
+			if(i%2==0)//this condition skiped when continue statment used
+	
+			{
+				continue;
+				
+			}
+			System.out.println("the value of i is :" +i);
+			
+		}
+	
+	        	int i=0;
+	        	
+	        	
+				do{
+					
+					if(i==3) {
+						
+						++i;
+						continue;
+					}
+						
+					System.out.println(i);
+					++i;
+				}while(i<10);
+					
+		
+			*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
